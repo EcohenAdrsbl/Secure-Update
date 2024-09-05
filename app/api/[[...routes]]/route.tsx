cf149f11aef6c3b485627499f77e74227971cc68
Eliyahu Cohen <eliyahucohen@Eliyahus-MacBook-Air.local>
2024-09-05 11:07:06 -0400

    first commit

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,41 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  basePath: '/api',
+  // hub: neynar({ apiKey: process.env.NEYMAR_API_KEY }),
+  browserLocation: '/',
+  imageOptions: {
+    fonts: [
+      {
+        name: 'JetBrains Mono',
+        source: 'google',
+      },
+    ],
+  },
+  title: 'Max ROI Post',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: 'https://i.postimg.cc/3xLSGhtS/AIPAC.jpg',
+    intents: [
+      <Button.Link href = 'https://www.addressable.io/product-updates/soc-2-certified'>Big News</Button.Link>,
+    ],
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
